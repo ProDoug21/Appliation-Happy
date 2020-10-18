ad26e9b5d7f64a57c7cccafe75051ff643f1791f
Douglas Jose <josed3851@gmail.com>
2020-10-18 03:33:44 -0300

    Dia 02

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,49 @@
+const options = {
+  dragging: false,
+  touchZone: false,
+  doubleClickzoom: false,
+  scrollwheelzoom: false,
+  zoomControl: false,
+};
+
+// create map
+const map = L.map("mapid", options).setView([-27.2224718, -49.6493543], 15);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [171, 2],
+});
+
+L.marker([-27.2224718, -49.6493543], { icon }).addTo(map).bindPopup(popup);
+
+/* IMAGE GALLERY*/
+
+function selectImage(event) {
+  const button = event.currentTarget
+  console.log(button.children)
+
+  // remover as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  //selecionar a imagem clicada
+
+  const image = button.children[0]
+  const imageContainer = document.querySelector(".orphanege-details > img")
+
+  //atualizar o conteiner de image
+
+  imageContainer.src = image.src
+
+  // adicionar a classe .active para esse botão
+
+  button.classList.add("active")
+}
